c10f9a8fd2c77835b00142907dab71bcf7e148c1
rory jackson <roryjamesjackson@hotmail.com>
2023-06-09 15:27:40 +0100

    linked the buttons to contact page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -108,9 +108,11 @@ const Home = () => {
             <h3>Our first one!</h3>
             <h2>Take a look at our <span className='highlighted-word'>first</span> project.</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <button className='button'>
-              Enquire now
-            </button>
+            <Link to="/contact">
+              <button className='button'>
+                Enquire now
+              </button>
+            </Link>
           </div>
         </div>
       </div>
