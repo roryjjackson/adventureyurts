8c76c20da9957dc11a44f4ac552a4b04be905f43
rory jackson <roryjamesjackson@hotmail.com>
2023-06-09 15:24:58 +0100

    styled the contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './Contact.css';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', { name, email, message });
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return(
-    <div>Contact page</div>
+    <div class="contact-form">
+      <div className='contact-form-content'>
+        <h3>Contact</h3>
+        <h1>We'd Love To Hear From You</h1>
+        <p>If you have any questions about pricing, delivery, construction etc etc, feel free to let us know below</p>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className='form-input'>
+              <label>Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <br />
+            <div className='form-input'>
+              <label>Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <br />
+            <div className='form-input message'>
+              <label>Message</label>
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+            </div>
+            <br />
+            <button type="submit" className='button'>Submit</button>
+          </form>
+        </div>
+      </div>
+      <div className='contact-form-image-container'>
+        <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1686312461/WhatsApp_Image_2023-05-19_at_19.58.42_b1dsbw.jpg' alt='' className='contact-image'></img>
+      </div>
+    </div>
   );
 }
 export default Contact;
