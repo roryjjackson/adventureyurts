226e8a1dea134ed8e540be3a40fe0b3e814c57b8
rory jackson <roryjamesjackson@hotmail.com>
2023-06-09 12:56:05 +0100

    home page done almost

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './Footer.css';
 
 const Footer = () => {
   return(
-    <div>Footer page</div>
+    <div className='footer'>
+      <div className='footer-content-container'>
+        <p>Copyright @AdventureYurts</p>
+        <p>Adventure Yurts</p>
+      </div>
+    </div>
   );
 }
 export default Footer;
