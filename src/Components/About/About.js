6e4909852015463606d8082e19a214e8290add8a
rory jackson <roryjamesjackson@hotmail.com>
2023-06-09 10:06:13 +0100

    basic styling of navbar

---
+++ src/Components/About/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const About = () => {
+  return(
+    <div>About page</div>
+  );
+}
+export default About;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        AV
+      </div>
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink to="/about" activeClassName="active">About Us</NavLink>
+        </li>
+        <li className="button">
+          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
