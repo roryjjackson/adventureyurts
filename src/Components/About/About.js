ab33f809d15c5dc749f52d595834c4898dd8c0ff
rory jackson <roryjamesjackson@hotmail.com>
2023-06-09 14:27:44 +0100

    testing production

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
       <div className='about-info-container'>
         <h2>About us</h2>
         <h1>A <span className='highlighted-word'>unique</span> team, a unique mission</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <p>Lorem Ipsumm is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </div>
       <div className='about-image-container'>
         <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1686312461/WhatsApp_Image_2023-06-01_at_09.40.50_w1rk2n.jpg' alt='' className='about-image'></img>
