1a8edb0728f7a0adf128abf37157d7be2c1856ae
rory jackson <roryjamesjackson@hotmail.com>
2023-06-12 11:31:33 +0100

    text for about us

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,12 +8,24 @@ const About = () => {
       <div className='about-info-container'>
         <h2>About us</h2>
         <h1>A <span className='highlighted-word'>unique</span> team, a unique mission</h1>
-        <p>Lorem Ipsumm is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <p>We both have grown up enjoying outdoor spaces. During the emergence of the Covid-19 pandemic, we set about looking at ways we could spend more time outdoors. So began the adventure of creating our own wooden yurt.</p>
+        <br/>
+        <p>Since then, it has been a long 3 year process to get the design absolutely bang on.</p>
+        <br/>
+        <p>blah blah blah?</p>
       </div>
       <div className='about-image-container'>
-        <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1686312461/WhatsApp_Image_2023-06-01_at_09.40.50_w1rk2n.jpg' alt='' className='about-image'></img>
-        <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1686307576/WhatsApp_Image_2023-05-24_at_12.05.43_l5jgli.jpg' alt='' className='about-image drop'></img>
-        <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1666713856/rory_zoeandcam_a29pkj_qdktj3.jpg' alt='' className='about-image'></img>
+        <div className='about-content'>
+          <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1686312461/WhatsApp_Image_2023-06-01_at_09.40.50_w1rk2n.jpg' alt='' className='about-image'></img>
+          <h2>Ronan</h2>
+          <p>Ronan has the practical skills required to set up a project. etc etc</p>
+        </div>
+
+        <div className='about-content'>
+          <img src='https://res.cloudinary.com/dfipoufmj/image/upload/v1666713856/rory_zoeandcam_a29pkj_qdktj3.jpg' alt='' className='about-image'></img>
+          <h2>Rory</h2>
+          <p>The son, utlizing the latest online avenues to help the business grow. etc etc</p>
+        </div>
       </div>
     </div>
   );
